b8833def93cf0effa41407ffa71df49d84d7ecb9
Nikita <blueprint80@mail.ru>
2021-11-30 23:11:37 +0300

    add rus alphabet list

--- Secret code.py
+++ Secret code.py
@@ -1,6 +1,10 @@
 message = input().upper()
 letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
            'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+letters_rus = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У',
+               'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я']
+
 inverted = letters[::-1]
 
 
@@ -10,6 +14,7 @@ def convertor(char):
         return inverted[n]
     else:
         return " "
+
 
 result = list(map(convertor, message))
 print("".join(result).lower())
